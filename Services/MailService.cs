021c51f15e62972a32bcda3101799ee02f5eb312
github-675455 <github-675455@vinicius.sl>
2019-04-27 06:48:09 -0300

    Remove transaction sql causing exception on executing multiple threads.

--- Services/MailService.cs
+++ Services/MailService.cs
@@ -33,7 +33,8 @@ namespace AliasMailApi.Services
             IMapper mapper,
             IMailgunAttachment mailgunAttachment,
             IMailboxService mailboxService,
-            ILogger<MailService> logger){
+            ILogger<MailService> logger)
+        {
             _context = context;
             _messageService = messageService;
             _domainService = domainService;
@@ -43,15 +44,16 @@ namespace AliasMailApi.Services
             _logger = logger;
         }
 
-        public async Task SetMailError(Mail mail, string errorMessage) {
+        public async Task SetMailError(Mail mail, string errorMessage)
+        {
 
             mail.JobStats = JobStats.Error;
 
-            if(errorMessage.Length > 4096)
+            if (errorMessage.Length > 4096)
                 errorMessage = errorMessage.Substring(0, 4096);
             else
                 mail.ErrorMessage = errorMessage;
-            
+
             mail.ErrorDate = DateTime.Now;
             mail.Retries = mail.Retries - 1;
             mail.NextRetry = DateTime.Now.AddMinutes(10);
@@ -63,36 +65,39 @@ namespace AliasMailApi.Services
         public async Task<BaseResponse<BaseMessage>> import(BaseMessage message)
         {
             var response = new BaseResponse<BaseMessage>();
-            if(message == null){
+            if (message == null)
+            {
                 _logger.LogInformation("Parameter is null");
-                response.Errors.Add(new ApiError{ description = "Parameter is null" });
+                response.Errors.Add(new ApiError { description = "Parameter is null" });
                 return response;
             }
-            
-            if(!message.Valid)
+
+            if (!message.Valid)
             {
                 _logger.LogInformation("Message {Id} is not valid by provider, so we cannot process it", message.Id);
-                response.Errors.Add(new ApiError{ description = "Message is not valid by provider, so we cannot process it" });
+                response.Errors.Add(new ApiError { description = "Message is not valid by provider, so we cannot process it" });
                 return response;
             }
-            
-            if(message is MailgunMessage)
+
+            if (message is MailgunMessage)
             {
                 var mailgunMessage = (MailgunMessage)message;
 
                 Mail newMail = null;
-                
+
                 try
                 {
                     newMail = await _context.Mails.AsNoTracking().FirstOrDefaultAsync(e => e.BaseMessageId == message.Id);
 
-                    if(newMail == null){
+                    if (newMail == null)
+                    {
                         newMail = new Mail(message);
                         await _context.Mails.AddAsync(newMail);
                         await _context.SaveChangesAsync();
                     }
 
-                    if(newMail.Retries <= 0){
+                    if (newMail.Retries <= 0)
+                    {
                         _logger.LogError("Too many reprocessed");
                         return response;
                     }
@@ -100,75 +105,73 @@ namespace AliasMailApi.Services
                     var toRecipient = new MailAddress(mailgunMessage.Recipient);
                     var domainFound = await _domainService.get(toRecipient.Host);
 
-                    if(domainFound == null){
+                    if (domainFound == null)
+                    {
                         //mark to delete mailgunmMessage
                         var errorMessage = string.Format("message id: {0} the domain {1} was not found", message.Id, toRecipient.Host);
                         await SetMailError(newMail, errorMessage);
                         _logger.LogError(errorMessage);
-                        response.Errors.Add(new ApiError{ description = "Domain not found" });
+                        response.Errors.Add(new ApiError { description = "Domain not found" });
                         return response;
                     }
 
                     var mailboxFound = await _mailboxService.GetMailbox(toRecipient.Address);
-                    
-                    if(mailboxFound == null)
+
+                    if (mailboxFound == null)
                     {
                         var newMailbox = _mailboxService.CreateDefaultMailbox(toRecipient, domainFound);
                         mailboxFound = await _mailboxService.CreateMailbox(newMailbox);
                     }
 
                     var mail = _mapper.Map<Mail>(mailgunMessage);
-                    
+
                     mail.Id = newMail.Id;
                     mail.BaseMessageId = newMail.BaseMessageId;
 
                     _context.Entry(newMail).State = EntityState.Detached;
 
-                    using(var transaction = _context.Database.BeginTransaction())
+                    if (mail.Attachments.NotEmpty())
                     {
-                        if(mail.Attachments.NotEmpty())
+                        var jsonAttachments = mail.Attachments.Replace("\\\"", "\"");
+                        var attachments = JsonConvert.DeserializeObject<MailAttachment[]>(jsonAttachments);
+                        ICollection<Task<MailAttachment>> getFiles = new List<Task<MailAttachment>>();
+
+                        attachments.ToList().ForEach(e =>
                         {
-                            var jsonAttachments = mail.Attachments.Replace("\\\"", "\"");
-                            var attachments = JsonConvert.DeserializeObject<MailAttachment[]>(jsonAttachments);
-                            ICollection<Task<MailAttachment>> getFiles = new List<Task<MailAttachment>>();
+                            getFiles.Add(_mailgunAttachment.get(e));
+                            e.MailId = mail.Id;
+                            e.url = string.Empty;
+                        });
+                        await Task.WhenAll(getFiles.ToArray());
 
-                            attachments.ToList().ForEach(e => {
-                                getFiles.Add(_mailgunAttachment.get(e));
-                                e.MailId = mail.Id;
-                                e.url = string.Empty;
-                            });
-                            await Task.WhenAll(getFiles.ToArray());
-
-                            getFiles.ToList().ForEach(async attachment =>
-                            {
-                                attachment.Result.Id = Guid.NewGuid();
-                                await _context.MailAttachments.AddAsync(attachment.Result);
-                                await _context.SaveChangesAsync();
-                            });
-                            mail.MailAttachmentsJobStatus = JobStats.Done;
-                        }
-
-                        mail.ErrorMessage = string.Empty;
-                        mail.ErrorDate = null;
-                        mail.NextRetry = null;
-
-                        mail.JobStats = JobStats.Done;
+                        getFiles.ToList().ForEach(async attachment =>
+                        {
+                            attachment.Result.Id = Guid.NewGuid();
+                            await _context.MailAttachments.AddAsync(attachment.Result);
+                            await _context.SaveChangesAsync();
+                        });
                         mail.MailAttachmentsJobStatus = JobStats.Done;
-
-                        _context.Mails.Update(mail);
-
-                        await _context.SaveChangesAsync();
-                        
-                        transaction.Commit();
                     }
+
+                    mail.ErrorMessage = string.Empty;
+                    mail.ErrorDate = null;
+                    mail.NextRetry = null;
+
+                    mail.JobStats = JobStats.Done;
+                    mail.MailAttachmentsJobStatus = JobStats.Done;
+
+                    _context.Mails.Update(mail);
+
+                    await _context.SaveChangesAsync();
+
                 }
-                catch(Exception exception)
+                catch (Exception exception)
                 {
                     var errorMessage = string.Format("{0} - {1}", exception.Message, exception.StackTrace);
                     await SetMailError(newMail, errorMessage);
                     _logger.LogError(errorMessage);
                     response.Success = false;
-                    response.Errors.Add(new ApiError{ description = errorMessage });
+                    response.Errors.Add(new ApiError { description = errorMessage });
                 }
             }
 
